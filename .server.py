aa3e41b69a7220de926c3d9149b1e3cdb55a3cbb
mental <m3nta1@yahoo.com>
2018-11-29 08:02:53 +0000

    include server app for debugging

---
+++ .server.py
@@ -0,0 +1,18 @@
+import flask
+
+app = flask.Flask(__name__, static_folder='./assets')
+game = flask.Blueprint('game', __name__ + '_game', url_prefix='/game')
+
+@game.route('/<fn>')
+def game_root(fn):
+    with open('game/%s' % fn) as inf:
+        return inf.read()
+
+@app.route('/')
+def main():
+    with open('index.html') as inf:
+        return inf.read()
+
+if __name__ == '__main__':
+    app.register_blueprint(game)
+    app.run('localhost', 80)
