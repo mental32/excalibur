a9985f90282e8acc1693c03b8330e1180082ac05
mental <m3nta1@yahoo.com>
2018-12-07 20:19:01 +0000

    bugfix check for negative coords and undefined tiles

--- game/effect.js
+++ game/effect.js
@@ -39,8 +39,11 @@ class MouseEffect extends Effect {
         let x = Math.floor(this.sketch.mouseX / 50);
         let y = Math.floor(this.sketch.mouseY / 50);
 
+        if (y < 0 || x < 0) return 0;
+
         if (this.y_ != y || this.x_ != x) {
-            window.state.map[this.y_][this.x_].clear().update();
+            let t = window.state.map[this.y_][this.x_];
+            if (t) t.clear().update();
             this.y_ = y;
             this.x_ = x;
         }
